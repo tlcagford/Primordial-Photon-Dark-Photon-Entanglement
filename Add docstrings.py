c0ad5022027e813570ec95b4c71e50a2f861f459
Tony E Ford <tlcagford@gmail.com>
2025-11-16 08:12:59 -0700

    Add files via upload

---
+++ Add docstrings.py
@@ -0,0 +1,16 @@
+def H_ms(k, params):
+    """
+    Compute the mixing Hamiltonian for photon-dark photon system.
+    
+    Parameters
+    ----------
+    k : float
+        Comoving momentum
+    params : dict
+        Physical parameters including masses and mixing
+    
+    Returns
+    -------
+    H : ndarray
+        2x2 Hamiltonian matrix
+    """
